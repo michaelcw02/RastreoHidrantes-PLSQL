d71c4cddde57a6a4de38dc022b25be58de3e4944
Michael Chen W <michael.cw02@hotmail.com>
2017-06-17 00:30:56 -0600

    jaja

--- menu.sql
+++ menu.sql
@@ -7,16 +7,12 @@ PROMPT 0: Salir.
 ACCEPT SELECTION PROMPT "Digite 0 - 1: "
 
 SET TERM OFF
-
-COLUMN SCRIPT NEW_VALUE V_SCRIPT 
-
-SELECT CASE '&op'
-        WHEN '1' THEN 'C:\Users\micha\Desktop\RastreoHidrantes-PLSQL\busqueda_hidrantes'
-        WHEN '0' THEN 'C:\Users\micha\Desktop\RastreoHidrantes-PLSQL\exit'
-        ELSE 'C:\Users\micha\Desktop\RastreoHidrantes-PLSQL\menu'
-        END AS SCRIPT
-FROM DUAL;
-
+        COLUMN SCRIPT NEW_VALUE v_script 
+        SELECT CASE '&op'
+                WHEN "1" THEN 'C:\Users\micha\Desktop\RastreoHidrantes-PLSQL\busqueda_hidrantes'
+                WHEN "0" THEN 'C:\Users\micha\Desktop\RastreoHidrantes-PLSQL\exit'
+                ELSE 'C:\Users\micha\Desktop\RastreoHidrantes-PLSQL\menu'
+                END AS SCRIPT
+        FROM DUAL;
 SET TERM ON
-
-@&V_SCRIPT.
+@&v_script.
