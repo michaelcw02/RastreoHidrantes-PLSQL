6a2c54dad0daea2c7227378982667e72f2ac8b87
Michael Chen W <michael.cw02@hotmail.com>
2017-06-17 00:20:20 -0600

    menu

---
+++ menu.sql
@@ -0,0 +1,22 @@
+SHOW USER
+
+PROMPT Por favor escoja una de las siguientes opciones:
+PROMPT 1: Buscar Hidrantes Cercanos.
+PROMPT 0: Salir.
+
+ACCEPT SELECTION PROMPT "Digite 0 - 1: "
+
+SET TERM OFF
+
+COLUMN SCRIPT NEW_VALUE V_SCRIPT 
+
+SELECT CASE '&op'
+        WHEN '1' THEN 'C:\Users\micha\Desktop\RastreoHidrantes-PLSQL\busqueda_hidrantes'
+        WHEN '0' THEN 'C:\Users\micha\Desktop\RastreoHidrantes-PLSQL\exit'
+        ELSE 'C:\Users\micha\Desktop\RastreoHidrantes-PLSQL\menu'
+        END AS SCRIPT
+FROM DUAL;
+
+SET TERM ON
+
+@&V_SCRIPT.
